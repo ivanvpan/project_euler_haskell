ab24a1b06b1a7b6ec0303971bea0a1afc63f4bd1
Ivan Pantuyev <ipantuyev@wgen.net>
2014-06-22 18:31:46 -0600

    5 update

--- 5/5.hs
+++ 5/5.hs
@@ -1,33 +1,38 @@
 import qualified Data.Map as Map
 import Data.Maybe (fromJust)
 
-factors :: Integral a => a -> Map.Map a [a]
-factors 0 = Map.empty
-factors 1 = Map.empty
-factors n = 
-    let results = factors (n `div` f)
+-- 1. factors as map fac -> [fac, fac, fac...]
+-- 2. combine factors from all number into an array [(fac, [fac, fac, fac...])]
+-- 3. keep factors maps with high occurence by pushing back into map
+-- Damn it's hard to read. *scratchhead*
+
+factorFrequency :: Integral a => a -> Map.Map a [a]
+factorFrequency 0 = Map.empty
+factorFrequency 1 = Map.empty
+factorFrequency n = 
+    let results = factorFrequency (n `div` f)
     in
         case Map.lookup f results of
             Nothing -> Map.insert f [f] results
             Just xs -> Map.insert f (f:xs) results
     where f = head $ filter (\d -> n `mod` d == 0) [2..n]
 
+commonFactors :: Integral a => [a] -> Map.Map a [a]
 commonFactors numbers = foldl pickFactors Map.empty (allFactors numbers)
     where
         allFactors :: Integral a => [a] -> [(a, [a])]
-        allFactors numbers = (foldl (++) [] (map (Map.toList . factors) numbers))
+        allFactors numbers = (foldl (++) [] (map (Map.toList . factorFrequency) numbers))
+
         pickFactors :: Integral a => Map.Map a [a] -> (a, [a]) -> Map.Map a [a]
-        pickFactors acc current =
-            case Map.lookup (fst current) acc of
-                Just xs ->
-                    if length (snd current) > length xs
-                        then Map.insert (fst current) (snd current) acc
-                        else acc
-                Nothing -> Map.insert (fst current) (snd current) acc
+        pickFactors acc current = Map.insertWith compare (fst current) (snd current) acc
+        compare new old =
+            if length new > length old
+                then new 
+                else old
 
 smallestDivisible :: Integral a => [a] -> a
-smallestDivisible numbers = foldl (*) 1 concat
+smallestDivisible numbers = foldl (*) 1 concatFactors
     where
-        concat = foldl (\acc x -> (snd x) ++ acc) [] (Map.toList $ commonFactors numbers)
+        concatFactors = foldl (\acc x -> (snd x) ++ acc) [] (Map.toList $ commonFactors numbers)
 
 main = print $ smallestDivisible [1..20]
